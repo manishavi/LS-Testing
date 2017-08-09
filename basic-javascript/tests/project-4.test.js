92d434a244ed244082d04ceb0492dc2aae603be8
manishavi <manishalal972@gmail.com>
2017-08-08 19:18:28 -0500

    working on project-4

--- basic-javascript/tests/project-4.test.js
+++ basic-javascript/tests/project-4.test.js
@@ -2,3 +2,30 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-4.js');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-4 Functions', () => {
+
+    describe('`multiplyArguments`', () => {
+        it ('should be function', () => {
+            const multiplyArguments = funcs.multiplyArguments;
+            assert.typeOf(multiplyArguments, 'function');
+        });
+        it('should be a number', () => {
+            const multiplyArguments = funcs.multiplyArguments;
+            const args = [1, 3, 4, 5];
+            assert.isNumber(multiplyArguments(args), 'number');
+        });
+        it('should contain the value', () =>{
+            const multiplyArguments = funcs.multiplyArguments;
+            const args = [1, 3, 4, 5];
+            assert.include(args, 4, 'value present');
+        });
+    });
+
+
+    describe('`invokeCallback`', () => {
+        it('should be a function', () => {
+            const invokeCallback = funcs.invokeCallback;
+            assert.typeOf(invokeCallback, 'function');
+        });
+    });
+});
